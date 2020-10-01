c595cc4be0ca140b721fdeacd78437791f4e9ddd
Richard Dern <richard.github@athaliasoft.com>
2020-10-01 13:43:15 +0200

    Clear cache when container starts

--- resources/container/entrypoint.sh
+++ resources/container/entrypoint.sh
@@ -6,6 +6,7 @@ if [ "$1" = "php-fpm" ]; then
     php /app/artisan migrate
     php /app/artisan storage:link
     php /app/artisan optimize
+    php /app/artisan cache:clear
     chown -R www-data:www-data /app/
 fi
 
