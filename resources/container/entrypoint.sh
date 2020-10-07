309a1b4707105df33b3a40caf2723bb45caa2415
Richard Dern <richard.github@athaliasoft.com>
2020-10-07 12:47:17 +0200

    Remove unnecessary theme install

--- resources/container/entrypoint.sh
+++ resources/container/entrypoint.sh
@@ -7,7 +7,6 @@ if [ "$1" = "php-fpm" ]; then
     php /app/artisan storage:link
     php /app/artisan optimize
     php /app/artisan cache:clear
-    php /app/artisan theme:install https://github.com/RichardDern/cyca_theme_dark
     chown -R www-data:www-data /app/
 fi
 
