621b8ed1db18ea140493ea81b1fb356f1bb570c1
Richard Dern <richard.github@athaliasoft.com>
2020-12-13 16:22:12 +0100

    Render some <details> elements stateful

---
+++ resources/js/components/StatefulDetails.vue
@@ -0,0 +1,23 @@
+<template>
+    <details v-bind:open="opened" v-on:toggle="onToggle"><slot></slot></details>
+</template>
+
+<script>
+export default {
+    props: ["name"],
+    computed: {
+        opened: function () {
+            return localStorage.getItem(this.name);
+        },
+    },
+    methods: {
+        onToggle: function (event) {
+            if (event.target.open) {
+                localStorage.setItem(this.name, event.target.open);
+            } else {
+                localStorage.removeItem(this.name);
+            }
+        },
+    },
+};
+</script>
