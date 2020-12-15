199f297787490d4913855e0f3a297056c0dd435e
Richard Dern <richard.github@athaliasoft.com>
2020-12-16 00:34:25 +0100

    Single responsibility methods

--- app/Analyzers/PDF.php
+++ app/Analyzers/PDF.php
@@ -2,26 +2,79 @@
 
 namespace App\Analyzers;
 
-use \Smalot\PdfParser\Parser;
-use Storage;
+use Smalot\PdfParser\Parser;
 
 /**
- * Extract information from a PDF file
+ * Extract information from a PDF file.
  */
 class PDF extends Analyzer
 {
+    /**
+     * PDF parser.
+     *
+     * @var \Smalot\PdfParser\Parser
+     */
+    protected $parser;
+
+    /**
+     * Analyzes document.
+     */
     public function analyze()
     {
-        $storageRoot      = $this->document->getStoragePath();
-        $metaFilename     = $storageRoot . '/meta.json';
-        $parser           = new Parser();
-        $pdf              = $parser->parseContent($this->body);
-        $details          = $pdf->getDetails();
+        $this->extractDetails();
+        $this->storeDetailsOnDisk();
+        $this->applyDetailsToDocument();
+    }
 
-        Storage::put($metaFilename, json_encode($details));
+    /**
+     * Store some details in database. This method uses an array to map document
+     * properties to metadata properties.
+     *
+     * @param mixed $mappings
+     */
+    protected function applyDetailsToDocument($mappings)
+    {
+        $mappings = [
+            'title' => 'Title',
+        ];
 
-        if (!empty($details['Title'])) {
-            $this->document->title = $details['Title'];
+        parent::applyDetailsToDocument($mappings);
+    }
+
+    /**
+     * Return an instance of PDF parser.
+     *
+     * @return \Smalot\PdfParser\Parser
+     */
+    private function getParser()
+    {
+        if (!$this->parser) {
+            $this->parser = new Parser();
         }
+
+        return $this->parser;
+    }
+
+    /**
+     * Parse PDF content.
+     *
+     * @param mixed $content
+     */
+    private function parseContent($content)
+    {
+        $parser = $this->getParser();
+
+        return $parser->parseContent($content);
+    }
+
+    /**
+     * Return an array of meta data included in PDF.
+     */
+    private function extractDetails()
+    {
+        $data          = $this->parseContent($this->body);
+        $this->details = $data->getDetails();
+
+        return $this->details;
     }
 }
